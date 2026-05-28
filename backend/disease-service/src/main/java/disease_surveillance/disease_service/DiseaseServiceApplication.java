8ee31b6f18ff8ed7604e935bf2e1a41077a9b8ad
Oliverking777 <oliviertwi@gmail.com>
2026-05-28 23:36:39 +0100

    fix: update datasource and rabbitmq credentials to match Docker config

---
+++ backend/disease-service/src/main/java/disease_surveillance/disease_service/DiseaseServiceApplication.java
@@ -0,0 +1,13 @@
+package disease_surveillance.disease_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiseaseServiceApplication.class, args);
+	}
+
+}
